58a84ba0039c32e328b86ec0509042f7d0fecfc8
Justin Webb <webb.n.justin@gmail.com>
2020-07-12 20:23:17 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,35 @@
 def addition(num1, num2)
-
+  add = (num1+num2)
+  puts add
+  return add
 end
 
 def subtraction(num1, num2)
-
+  sub = (num1 - num2)
+  puts sub
+  return sub
 end
 
 def division(num1, num2)
-
+  div = (num1/num2)
+  puts div
+  return div
 end
 
 def multiplication(num1, num2)
-
+  mul = (num1 * num2)
+  puts mul
+  return mul
 end
 
 def modulo(num1, num2)
-
+ mod = (num1%num2)
+ puts mod
+ return mod
 end
 
 def square_root(num)
-
+  root = Math.sqrt(num)
+  puts root 
+  return root
 end
